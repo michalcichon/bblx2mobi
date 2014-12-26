9549f21f470540bcd7e5eeec2dd53e405bc66515
Michal Cichon <michal.cichon@aol.com>
2014-12-26 23:09:07 +0100

    dictionary - old testament

--- bblx2mobi.js
+++ bblx2mobi.js
@@ -4,6 +4,49 @@ var fs = require('fs'),
 	lang = process.argv[3],
 	db = null;
 
+var books = new Array(
+	'',
+	'Księga Rodzaju', 
+	'Księga Wyjścia', 
+	'Księga Kapłańska', 
+	'Księga Liczb', 
+	'Księga Powtórzonego Prawa', 
+	'Księga Jozuego', 
+	'Księga Sędziów', 
+	'Księga Rut', 
+	'1 Księga Samuela', 
+	'2 Księga Samuela', 
+	'1 Księga Królewska', 
+	'2 Księga Królewska', 
+	'1 Księga Kronik', 
+	'2 Księga Kronik', 
+	'Księga Ezdrasza', 
+	'Księga Nehemiasza', 
+	'Księga Estery', 
+	'Księga Hioba', 
+	'Księga Psalmów', 
+	'Księga Przysłów', 
+	'Księga Koheleta', 
+	'Pieśń nad pieśniami', 
+	'Księga Izajasza', 
+	'Księga Jeremiasza', 
+	'Lamentacje Jeremiasza', 
+	'Księga Ezechiela', 
+	'Księga Daniela', 
+	'Księga Ozeasza'
+	'Księga Joela',
+	'Księga Amosa',
+	'Księga Abdiasza',
+	'Księga Jonasza',
+	'Księga Micheasza',
+	'Księga Nahuma',
+	'Księga Habakuka',
+	'Księga Sofoniasza',
+	'Księga Aggeusza',
+	'Księga Zachariasza',
+	'Księga Malachiasza'
+	);
+
 function checkArgs() {
 	var usage = '* usage:\tnode bblx2mobi <filename> <lang>\n* example:\tnode bblx2mobi bible.bblx pl';
 	if (bblxFilename === undefined) {
