5f2c249e343c2ac5de58779f77b0583b5db73bd0
Michal Cichon <michal.cichon@aol.com>
2015-01-02 03:41:29 +0100

    configs

--- bblx2mobi.js
+++ bblx2mobi.js
@@ -13,9 +13,33 @@ var fs = require('fs'),
 
 var books_pl = new Array('','Księga Rodzaju','Księga Wyjścia','Księga Kapłańska','Księga Liczb','Księga Powtórzonego Prawa','Księga Jozuego','Księga Sędziów','Księga Rut','1 Księga Samuela',  '2 Księga Samuela','1 Księga Królewska','2 Księga Królewska','1 Księga Kronik','2 Księga Kronik','Księga Ezdrasza','Księga Nehemiasza','Księga Estery','Księga Hioba','Księga Psalmów','Księga Przysłów','Księga Koheleta','Pieśń nad pieśniami','Księga Izajasza','Księga Jeremiasza','Lamentacje Jeremiasza','Księga Ezechiela','Księga Daniela','Księga Ozeasza','Księga Joela','Księga Amosa','Księga Abdiasza','Księga Jonasza','Księga Micheasza','Księga Nahuma','Księga Habakuka','Księga Sofoniasza','Księga Aggeusza','Księga Zachariasza','Księga Malachiasza','Ewangelia wg. św. Mateusza','Ewangelia wg. św. Marka','Ewangelia wg. św. Łukasza','Ewangelia wg. św. Jana','Dzieje Apostolskie','List do Rzymian','1 List do Koryntian','2 List do Koryntian','List do Galatów','List do Efezjan','List do Filipian','List do Kolosan','1 List do Tesaloniczan','2 List do Tesaloniczan','1 List do Tymoteusza','2 List do Tymoteusza','List do Tytusa','List do Filemona','List do Hebrajczyków','List św. Jakuba','1 List św. Piotra','2 List św. Piotra','1 List św. Jana','2 List św. Jana','3 List św. Jana','List św. Judy','Apokalipsa św. Jana'),
 	books_en = new Array('','Genesis','Exodus','Leviticus','Numbers','Deuteronomy','Joshua','Judges','Ruth','1 Samuel','2 Samuel','1 Kings','2 Kings','1 Chronicles','2 Chronicles','Ezra','Nehemiah','Esther','Job','Psalm','Proverbs','Ecclesiastes','Song of Solomon','Isaiah','Jeremiah','Lamentations','Ezekiel','Daniel','Hosea','Joel','Amos','Obadiah','Jonah','Micah','Nahum','Habakkuk','Zephaniah','Haggai','Zechariah','Malachi','Matthew','Mark','Luke','John','Acts','Romans','1 Corinthians','2 Corinthians','Galatians','Ephesians','Philippians','Colossians','1 Thessalonians','2 Thessalonians','1 Timothy','2 Timothy','Titus','Philemon','Hebrews','James','1 Peter','2 Peter','1 John','2 John','3 John','Jude','Revelation'),
-	titles = {
-		'pl' : 'Pismo Święte',
-		'en' : 'Holy Bible'
+	configs = {
+		'pl' : {
+			'title': 'Pismo Święte',
+			'coverImage': 'cover.jpg',
+			'contents': 'Spis treści',
+			'creators': 'Bóg',
+			'creatorFileAs': 'BÓG',
+			'pubId': 'com.github.michalcichon.bblx2mobi.pismoSwiete',
+			'lang': 'pl-PL',
+			'modified': '2012-01-18T12:47:00Z',
+			'publisher': 'bblx2mobi',
+			'contributor': 'Michal Cichon',
+			'attributionURL': 'http://github.com/michalcichon/bblx2mobi'
+		},
+		'en' : {
+			'title': 'Holy Bible',
+			'coverImage': 'cover.jpg',
+			'contents': 'Contents',
+			'creators': 'God',
+			'creatorFileAs': 'GOD',
+			'pubId': 'com.github.michalcichon.bblx2mobi.pismoSwiete',
+			'lang': 'en-US',
+			'modified': '2012-01-18T12:47:00Z',
+			'publisher': 'bblx2mobi',
+			'contributor': 'Michal Cichon',
+			'attributionURL': 'http://github.com/michalcichon/bblx2mobi'
+		}
 	};
 
 function checkArgs() {
@@ -72,13 +96,25 @@ function prepareCover(tempDir) {
 			printError(err);
 		}
 
-		var rendered = mustache.render(data, {title: titles[lang], coverImage: 'cover.jpg'});
+		var rendered = mustache.render(data, {title: configs[lang].title, coverImage: configs[lang].coverImage});
 		fs.writeFile(tempDir + '/OPS/cover.xhtml', rendered, function(err) {
 			if(err) {
 				printError(err);
 			}
 		});
 	});
+}
+
+function prepareToc() {
+
+}
+
+function prepareChapters() {
+
+}
+
+function preparePackage() {
+
 }
 
 
